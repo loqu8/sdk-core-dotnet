21bc56681658df08ff6ab9cc30517409d0c48bd4
E. Timothy Uy <tim@loqu8.com>
2013-08-08 01:42:01 -0700

    Add Async name

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PayPal Core SDK for .NET")]
+[assembly: AssemblyTitle("PayPal Core SDK for .NET Async")]
 [assembly: AssemblyCompany("eBay Inc.")]
 [assembly: AssemblyProduct("PayPalCoreSDK")]
 [assembly: AssemblyCopyright("Copyright © eBay Inc., 2009-2013")]
